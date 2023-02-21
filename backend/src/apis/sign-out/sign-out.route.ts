eef431a03087bdcbc49aa14dd40239f2aa7e7053
Joel Rinderknecht <jrinderknecht@cnm.edu>
2023-02-21 16:20:07 -0700

    deleted signout files due to errors

--- backend/src/apis/sign-out/sign-out.route.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express'
-import { signOutController } from './sign-out.controller'
-
-export const SignOutRoute: Router = Router ()
-
-SignOutRoute.route('/')
-  .get(signOutController)
