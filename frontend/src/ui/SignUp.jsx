56cdb183a4df18e3d29f6247c8ae3b8e0bcca867
Joel Rinderknecht <jrinderknecht@cnm.edu>
2023-03-03 10:25:24 -0700

    added signup route

---
+++ frontend/src/ui/SignUp.jsx
@@ -0,0 +1,18 @@
+import {Container} from "react-bootstrap";
+
+
+export function About() {
+  return (
+    <>
+      <Navigation /> //Not created yet
+
+    <section>
+      <Container>
+
+      </Container>
+    </section>
+
+
+    </>
+  )
+}
