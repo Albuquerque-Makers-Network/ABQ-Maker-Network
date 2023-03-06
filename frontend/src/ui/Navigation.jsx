36ec3b1bf4b86cfce8525e15829cd36893fc58f9
Jordyn Palma <Jordynpalmadesign@gmail.com>
2023-03-06 11:41:25 -0700

    formatted nav bar added profile pic placeholder

--- frontend/src/ui/Navigation.jsx
+++ frontend/src/ui/Navigation.jsx
@@ -1,16 +1,22 @@
-import {Container, Nav, Navbar} from "react-bootstrap";
+import {Container, Image, Nav, Navbar} from "react-bootstrap";
 import React from "react";
+import Logo from "../assets/maker-network-logo.png"
+import ProfilePicture from "../assets/profile-placeholder.png"
 
 export function Navigation(){
     return (
         <>
-            <Navbar bg="secondary" variant="dark" expand="lg">
+            <Navbar bg="light" variant="light" expand="lg">
                 <Container fluid>
                     <Navbar.Toggle className='ms-auto'  aria-controls="basic-navbar-nav"/>
-                    <Navbar.Collapse id="basic-navbar-nav" >
-                        <Navbar.Brand className='px-2' href="/">Home</Navbar.Brand>
+                    <Navbar.Collapse  className='my-3 mx-5' id="basic-navbar-nav" >
+                        <Navbar.Brand href="/">
+                        <Image fluid src={Logo} alt='image of ABQ Maker Network logo. Text reads ABQ Maker Network.' width='100'/> ABQ Maker Network
+                        </Navbar.Brand>
                         <Nav className="ms-auto">
-                            <Nav.Link href="/about">About</Nav.Link>
+                            <Nav.Link href="/">Home</Nav.Link>
+                            <Nav.Link href="/sign-up">Sign In/Up</Nav.Link>
+                            <Nav.Link href="/sign-up"><Image fluid src={ProfilePicture} width='30'/></Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
