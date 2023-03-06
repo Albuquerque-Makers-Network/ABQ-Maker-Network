159a1f7e007b700aa5c04942418aa740630e8ffc
Joel Rinderknecht <jrinderknecht@cnm.edu>
2023-03-06 15:59:51 -0700

    created PortfolioImage.jsx

--- frontend/src/ui/maker-profile/MakerProfile.jsx
+++ frontend/src/ui/maker-profile/MakerProfile.jsx
@@ -1,45 +1,37 @@
 import {Col, Container, Image, Row} from "react-bootstrap";
+import {PortfolioImage} from "./PortfolioImage.jsx";
+import React from "react";
+import {Skill} from "./Skill.jsx";
+import "./MakerProfile.css"
 
 
 export function MakerProfile() {
   return (
     <>
-
-      {/*<Navigation />*/}
-
-  <Container className="bg-secondary mt-5 p-5 rounded-4 me-5">
+  <Container className="bg-light p-5 ps-lg-0 mt-5 mx-auto rounded-4">
     <Row>
-      <Col className="float-start">
-        <Image roundedCircle src="https://www.picsum.photos/250" alt="profile picture" className="d-block " />
+      <Col>
+        <Image roundedCircle src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="profile picture" className="d-block mx-auto mb-3" id="profile-image"/>
       </Col>
-      <Col lg={6}>
-        <h2>profileFullName</h2>
-        <p>profileAboutMe</p>
+      <Col lg={6} className="order-3 order-md-2">
+        <h2 className="text-center text-md-start">profileFullName</h2>
+        <p className="mt-3">profileAboutMe: As High as Honor. The winds of Winter. Never Resting. Can a man still be brave if he’s afraid? That is the only time a man can be brave.Forgive my manners. I don't see many ladies these days. Lucky for the ladies. A forked purple lightning bolt, on black field speckled with four-pointed stars. The tourney of Ashford Meadows. It's ten thousand miles between Kings landing and the wall.</p>
         <p>profileEmail</p>
       </Col>
-      <Col>
-        <h2>Skills:</h2>
-        <p className="rounded-4 bg-dark text-white text-center p-2  d-inline-block ">Skill 1</p>
-        {/*remove below*/}
-        <p className="rounded-4 bg-dark text-white text-center p-2  d-inline">Skill 1</p>
-        <p className="rounded-4 bg-dark text-white text-center p-2  d-inline">Skill 1</p>
-        <p className="rounded-4 bg-dark text-white text-center p-2  d-inline">Skill 1</p>
-        <p className="rounded-4 bg-dark text-white text-center p-2  d-inline">Skill 1</p>
-        <p className="rounded-4 bg-dark text-white text-center p-2  d-inline">Skill 1</p>
+      <Col className="order-2 order-md-3 my-3">
+        <h2 className="text-center text-lg-start ps-2">Skills:</h2>
+        <Skill />
       </Col>
     </Row>
   </Container>
-    <Container className="m-5">
+    <Container>
     <Row>
-      <Col className="container-fluid">
-        <Image src= "https://www.picsum.photos/150/200" alt="portfolio image" className="p-2 rounded-4"/>
-        <Image rounded src= "https://www.picsum.photos/150/200" alt="portfolio image" className="p-2"/>
-        <Image rounded src= "https://www.picsum.photos/150/200" alt="portfolio image" className="p-2"/>
-        <Image rounded src= "https://www.picsum.photos/150/200" alt="portfolio image" className="p-2"/>
+      <Col className="mx-5 my-5">
+        <PortfolioImage />
       </Col>
-      <Col lg={4} className="bg-secondary rounded-4 p-4">
-        <h2>Pricing:</h2>
-        <p>profilePricing</p>
+      <Col lg={4} className="bg-light rounded-4 p-4 mt-4">
+        <h2 className="text-center text-sm-start">Pricing:</h2>
+        <p className="mt-3">profilePricing: Jin ave sekke verven anni m'orvikoon. Hash yer dothrae chek asshekh? Ki fin yeni?</p>
       </Col>
     </Row>
   </Container>
---
+++ frontend/src/ui/maker-profile/PortfolioImage.jsx
@@ -0,0 +1,12 @@
+import {Image} from "react-bootstrap";
+import "./PortfolioImage.css";
+export function PortfolioImage () {
+  return (
+    <>
+      <Image src="https://www.picsum.photos/150/200" alt="portfolio image" className="p-2 rounded-4" id="portfolio-image"/>
+      <Image src="https://www.picsum.photos/150/200" alt="portfolio image" className="p-2 rounded-4" id="portfolio-image"/>
+      <Image src="https://www.picsum.photos/150/200" alt="portfolio image" className="p-2 rounded-4" id="portfolio-image"/>
+      <Image src="https://www.picsum.photos/150/200" alt="portfolio image" className="p-2 rounded-4" id="portfolio-image"/>
+    </>
+  )
+}
