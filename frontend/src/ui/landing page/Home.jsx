2d6c7a00ef14b0ad70880a08f7744efbc61a63f7
Jordyn Palma <Jordynpalmadesign@gmail.com>
2023-03-07 14:23:48 -0700

    completed landing page layout w/components. Added hover effects.

--- frontend/src/ui/landing page/Home.jsx
+++ frontend/src/ui/landing page/Home.jsx
@@ -8,18 +8,20 @@ export function Home() {
     return (
         <>
             <h2 className="text-center my-5">Welcome to the ABQ Maker Network!</h2>
-            <Container className="my-5">
+
+            <Container className="my-5 mx-auto px-md-0 px-4">
                 <h3 className="my-3">Ready to start collaborating with others?</h3>
+                <p>Start searching by maker information or skill type.</p>
                 <Search/>
             </Container>
 
-            <Container className="my-5">
+            <Container className="my-5 mx-auto px-md-0 px-4">
                 <h4 className="my-3">Popular Skills</h4>
                 <SkillIcons/>
             </Container>
 
-            <Container className="my-5">
-            <ProfileCards/>
+            <Container className="my-5 mx-auto px-md-0 px-4">
+                <ProfileCards/>
             </Container>
         </>
     )
--- frontend/src/ui/landing page/Search.jsx
+++ frontend/src/ui/landing page/Search.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import {Button, Col, Row, Container, Dropdown, DropdownButton, Form} from "react-bootstrap";
+import './LandingPage.css'
 
 export function Search() {
     return (
         <>
+            <Container fluid className="p-0">
             <Row>
-                <Col lg={11}>
+                <Col sm={10}>
                     <Form className="d-flex">
                             <Form.Control type="search" placeholder="Search" className="me-2" aria-label="Search"/>
                             <Button variant="primary">Search</Button>
                     </Form>
                 </Col>
-                <Col lg={1}>
-                    <DropdownButton title="All Skills">
+                <Col sm={2}>
+                    <DropdownButton id="dropdown-button" title="All Skills">
                             <Dropdown.Item>Skill</Dropdown.Item>
                     </DropdownButton>
                 </Col>
             </Row>
+            </Container>
         </>
     )
 }
