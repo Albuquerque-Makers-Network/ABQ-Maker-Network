6f3f0bb0c9c9445d3f319bbeccfd6126963942ea
Jordyn Palma <Jordynpalmadesign@gmail.com>
2023-03-07 15:46:58 -0700

    added landing page header image and styling

--- frontend/src/ui/landing page/Home.jsx
+++ frontend/src/ui/landing page/Home.jsx
@@ -2,27 +2,37 @@ import React from "react"
 import {Search} from "./Search.jsx";
 import {SkillIcons} from "./SkillIcons"
 import {ProfileCards} from "./ProfileCards.jsx";
-import {Container} from "react-bootstrap";
+import {Container, Image, Row, Col} from "react-bootstrap";
+import Logo from "../../assets/maker-network-logo.png";
 
 export function Home() {
     return (
         <>
-            <h2 className="text-center my-5">Welcome to the ABQ Maker Network!</h2>
+            <section id='intro'>
+                <Container id='intro-content'>
+                    <Container  className='text-center my-3'>
+                    <Image className="me-4" fluid src={Logo} alt='image of ABQ Maker Network logo. Text reads ABQ Maker Network.' width='150rem'/>
+                    <h2 className="text-center my-5">Welcome to the ABQ Maker Network!</h2>
+                    </Container>
 
-            <Container className="my-5 mx-auto px-md-0 px-4">
-                <h3 className="my-3">Ready to start collaborating with others?</h3>
-                <p>Start searching by maker information or skill type.</p>
-                <Search/>
-            </Container>
-
-            <Container className="my-5 mx-auto px-md-0 px-4">
-                <h4 className="my-3">Popular Skills</h4>
-                <SkillIcons/>
-            </Container>
-
-            <Container className="my-5 mx-auto px-md-0 px-4">
-                <ProfileCards/>
-            </Container>
+                    <Container className="my-5 mx-auto px-md-0 px-4">
+                        <h3 className="my-3">Ready to start collaborating with others?</h3>
+                        <p>Start searching by maker information or skill type.</p>
+                        <Search/>
+                    </Container>
+                </Container>
+            </section>
+            <section>
+                <Container className="my-5 mx-auto px-md-0 px-4">
+                    <h4 className="my-3">Popular Skills</h4>
+                    <SkillIcons/>
+                </Container>
+            </section>
+            <section>
+                <Container className="my-5 mx-auto px-md-0 px-4">
+                    <ProfileCards/>
+                </Container>
+            </section>
         </>
     )
 }
--- frontend/src/ui/landing page/Search.jsx
+++ frontend/src/ui/landing page/Search.jsx
@@ -10,11 +10,11 @@ export function Search() {
                 <Col sm={10}>
                     <Form className="d-flex">
                             <Form.Control type="search" placeholder="Search" className="me-2" aria-label="Search"/>
-                            <Button variant="primary">Search</Button>
+                            <Button variant="outline-light">Search</Button>
                     </Form>
                 </Col>
                 <Col sm={2}>
-                    <DropdownButton id="dropdown-button" title="All Skills">
+                    <DropdownButton variant="outline-light" id="dropdown-button" title="All Skills">
                             <Dropdown.Item>Skill</Dropdown.Item>
                     </DropdownButton>
                 </Col>
