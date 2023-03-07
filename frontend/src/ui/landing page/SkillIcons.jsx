ea13e1b43a0da43886a97869b1cf54b19c612a88
Jordyn Palma <Jordynpalmadesign@gmail.com>
2023-03-07 15:53:25 -0700

    adjusted breakpoints for skill icons on landing page

--- frontend/src/ui/landing page/SkillIcons.jsx
+++ frontend/src/ui/landing page/SkillIcons.jsx
@@ -20,43 +20,43 @@ export function SkillIcons() {
         <>
             <Container>
                 <Row id='skill-icons' className='gap-lg-3'>
-                    <Col xs={4} sm={3} md={2} lg={1} className='text-center p-1'>
+                    <Col xs={3} md={2} lg={1} className='text-center p-1'>
                         <Button type='submit' className='bg-transparent border-0'><Image fluid src={ThreePrint} alt='3d Printer Icon' width='100'/></Button>
                         <p>3d Printing</p>
                     </Col>
-                    <Col xs={4} sm={3} md={2} lg={1} className='text-center p-1'>
+                    <Col xs={3} md={2} lg={1} className='text-center p-1'>
                         <Button type='submit' className='bg-transparent border-0'><Image fluid src={Ceramics} alt='Icon of a vase' width='100'/></Button>
                         <p>Ceramics</p>
                     </Col>
-                    <Col xs={4} sm={3} md={2} lg={1} className='text-center p-1'>
+                    <Col xs={3} md={2} lg={1} className='text-center p-1'>
                         <Button type='submit' className='bg-transparent border-0'><Image fluid src={Woodworking} alt='Icon of a piece of wood and chisel' width='100'/></Button>
                         <p>Woodwork</p>
                     </Col>
-                    <Col xs={4} sm={3} md={2} lg={1} className='text-center p-1'>
+                    <Col xs={3} md={2} lg={1} className='text-center p-1'>
                         <Button type='submit' className='bg-transparent border-0'><Image fluid src={Metalworking} alt='Icon of an anvil' width='100'/></Button>
                         <p>Metalwork</p>
                     </Col>
-                    <Col xs={4} sm={3} md={2} lg={1} className='text-center p-1'>
+                    <Col xs={3} md={2} lg={1} className='text-center p-1'>
                         <Button type='submit' className='bg-transparent border-0'><Image fluid src={Welding} alt='Icon of a welding hood and torch' width='100'/></Button>
                         <p>Welding</p>
                     </Col>
-                    <Col xs={4} sm={3} md={2} lg={1} className='text-center p-1'>
+                    <Col xs={3} md={2} lg={1} className='text-center p-1'>
                         <Button type='submit' className='bg-transparent border-0'><Image fluid src={Laser} alt='Icon of a laser beam' width='100'/></Button>
                         <p>Laser Cutting</p>
                     </Col>
-                    <Col xs={4} sm={3} md={2} lg={1} className='text-center p-1'>
+                    <Col xs={3} md={2} lg={1} className='text-center p-1'>
                         <Button type='submit' className='bg-transparent border-0'><Image fluid src={Printing} alt='Icon of a printer' width='100'/></Button>
                         <p>Printing</p>
                     </Col>
-                    <Col xs={4} sm={3} md={2} lg={1} className='text-center p-1'>
+                    <Col xs={3} md={2} lg={1} className='text-center p-1'>
                         <Button type='submit' className='bg-transparent border-0'><Image fluid src={Jewelry} alt='Icon of a Ring' width='100'/></Button>
                         <p>Jewelry Making</p>
                     </Col>
-                    <Col xs={4} sm={3} md={2} lg={1} className='text-center p-1'>
+                    <Col xs={3} md={2} lg={1} className='text-center p-1'>
                         <Button type='submit' className='bg-transparent border-0'><Image fluid src={ScreenPrint} alt='Icon of a t-shirt' width='100'/></Button>
                         <p>Screen Printing</p>
                     </Col>
-                    <Col xs={4} sm={3} md={2} lg={1} className='text-center p-1'>
+                    <Col xs={3} md={2} lg={1} className='text-center p-1'>
                         <Button type='submit' className='bg-transparent border-0'><Image fluid src={Sewing} alt='Icon of a spool of thread and sewing needle' width='100'/></Button>
                         <p>Sewing</p>
                     </Col>
