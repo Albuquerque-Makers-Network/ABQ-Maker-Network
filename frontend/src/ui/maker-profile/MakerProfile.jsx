96c7fcc3bbbd282198c4b05948dbed7ae0a70b9a
Joel Rinderknecht <jrinderknecht@cnm.edu>
2023-03-09 16:22:29 -0700

    changed text colors

--- frontend/src/ui/maker-profile/MakerProfile.jsx
+++ frontend/src/ui/maker-profile/MakerProfile.jsx
@@ -12,33 +12,30 @@ export function MakerProfile() {
     <>
       <Container className="p-5 ps-lg-0 mt-5 mx-auto rounded-4" id="background-about-me">
         <Row>
-
           <Col className="d-flex align-content-center">
             <Image roundedCircle height={250} width={250} src="https://bootdey.com/img/Content/avatar/avatar7.png"
                    alt="profile picture" className=" d-block mx-auto mb-lg-0 mb-3" id="profile-image"/>
           </Col>
 
           <Col lg={6} className="order-3 order-sm-2 text-light">
-            <h1 className="text-center text-md-start">profileFullName</h1>
-            <p className="mt-3">profileAboutMe: As High as Honor. The winds of Winter. Never Resting. Can a
+            <h1 className="text-center text-md-start" id="profile-name">profileFullName</h1>
+            <p className="mt-3" id="about-me-text">profileAboutMe: As High as Honor. The winds of Winter. Never Resting. Can a
               man still be brave if he’s afraid? That is the only time a man can be brave.Forgive my manners. I don't
               see many ladies these days. Lucky for the ladies. A forked purple lightning bolt, on black field speckled
               with four-pointed stars. The tourney of Ashford Meadows. It's ten thousand miles between Kings landing and
               the wall.</p>
-            <p>profileEmail</p>
+            <p id="email-text">profileEmail</p>
           </Col>
 
           <Col className="order-2 order-sm-3 my-3 mt-lg-0 d-block d-lg-inline">
-            <h2 className="text-center text-lg-start text-light">Skills:</h2>
+            <h2 className="text-center text-lg-start text-light" id="skills-name">Skills:</h2>
             <Skill/>
           </Col>
-
         </Row>
       </Container>
 
       <Container>
         <Row>
-
           <Col className="my-3">
             <PortfolioImage/>
           </Col>
@@ -48,7 +45,6 @@ export function MakerProfile() {
             <p className=" mt-3">profilePricing: Jin ave sekke verven anni m'orvikoon. Hash yer dothrae chek asshekh?
               Ki fin yeni?</p>
           </Col>
-
         </Row>
       </Container>
     </>
