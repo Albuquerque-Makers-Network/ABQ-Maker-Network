94c8ec45f95d65651b1c95d881e9151f2b0c703e
Jordyn Palma <Jordynpalmadesign@gmail.com>
2023-03-10 08:50:59 -0700

    added 404 page styles

--- frontend/src/ui/FourOhFour.jsx
+++ frontend/src/ui/FourOhFour.jsx
@@ -1,9 +1,17 @@
 import React from "react"
+import {Button, Container} from "react-bootstrap";
+import "./App.css"
 
 export const FourOhFour = () => {
     return (
         <>
-            <h1>This is not the page you were looking for</h1>
+            <Container className='my-5' id='error'>
+                <Container fluid id='error-content'>
+                    <h1 id='fourOHfour'>404</h1>
+                    <h1>This is not the page you were looking for</h1>
+                    <Button className='my-3' href="/" variant="light">Go Back Home</Button>
+                </Container>
+            </Container>
         </>
     )
 };
