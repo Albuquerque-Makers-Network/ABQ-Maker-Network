2474436680325178c9a63d6613531c2b98190b25
Joel Rinderknecht <jrinderknecht@cnm.edu>
2023-03-13 16:52:09 -0600

    added auth and currentUser to store

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,7 +1,9 @@
+import auth from "./auth"
+import currentUser from "./currentUser"
 import profiles from "./profiles"
 
 import {configureStore,combineReducers} from "@reduxjs/toolkit";
 
-const reducer = combineReducers({profiles})
+const reducer = combineReducers({auth, currentUser, profiles})
 export const store = configureStore({reducer});
 
