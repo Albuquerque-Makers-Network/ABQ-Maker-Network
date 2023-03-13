a190abc6bfa2131ca8cd81a4964ca4f85ffa5746
Joel Rinderknecht <jrinderknecht@cnm.edu>
2023-03-13 16:40:21 -0600

    created auth.js file

--- frontend/src/ui/maker-profile/MakerProfile.jsx
+++ frontend/src/ui/maker-profile/MakerProfile.jsx
@@ -13,18 +13,18 @@ export function MakerProfile() {
       <Container className="p-5 ps-lg-0 mt-5 mx-auto rounded-4" id="background-about-me">
         <Row>
           <Col className="d-flex align-content-center">
-            <Image roundedCircle height={250} width={250} src="https://bootdey.com/img/Content/avatar/avatar7.png"
+            <Image roundedCircle height={250} width={250} src={profile.profileImageUrl}
                    alt="profile picture" className=" d-block mx-auto mb-lg-0 mb-3" id="profile-image"/>
           </Col>
 
           <Col lg={6} className="order-3 order-sm-2 text-light">
-            <h1 className="text-center text-md-start" id="profile-name">profileFullName</h1>
+            <h1 className="text-center text-md-start" id="profile-name">{profile.profileName}</h1>
             <p className="mt-3" id="about-me-text">profileAboutMe: As High as Honor. The winds of Winter. Never Resting. Can a
               man still be brave if he’s afraid? That is the only time a man can be brave.Forgive my manners. I don't
               see many ladies these days. Lucky for the ladies. A forked purple lightning bolt, on black field speckled
               with four-pointed stars. The tourney of Ashford Meadows. It's ten thousand miles between Kings landing and
               the wall.</p>
-            <p id="email-text">profileEmail</p>
+            <p id="email-text">{profile.profileEmail}</p>
           </Col>
 
           <Col className="order-2 order-sm-3 my-3 mt-lg-0 d-block d-lg-inline">
@@ -37,12 +37,12 @@ export function MakerProfile() {
       <Container>
         <Row>
           <Col className="my-3">
-            <PortfolioImage/>
+            <PortfolioImage />
           </Col>
 
           <Col xs={12} md={4} className="rounded-4 p-4 mt-4 mb-auto text-white" id="pricing">
             <h2 className="text-md-center text-sm-start">Pricing:</h2>
-            <p className=" mt-3">profilePricing: Jin ave sekke verven anni m'orvikoon. Hash yer dothrae chek asshekh?
+            <p className=" mt-3">{profile.profilePricing} Jin ave sekke verven anni m'orvikoon. Hash yer dothrae chek asshekh?
               Ki fin yeni?</p>
           </Col>
         </Row>
--- frontend/src/ui/maker-profile/PortfolioImage.jsx
+++ frontend/src/ui/maker-profile/PortfolioImage.jsx
@@ -1,11 +1,18 @@
 import {Image} from "react-bootstrap";
-export function PortfolioImage () {
+import {useDispatch} from "react-redux";
+export const PortfolioImage = ({ portfolioImage }) => {
+
+  const dispatch = useDispatch()
+
+
+
   return (
     <>
-      <Image src="https://www.picsum.photos/150/200" alt="portfolio image" className="p-2 rounded-4" id="portfolio-image"/>
-      <Image src="https://www.picsum.photos/250/200" alt="portfolio image"  className="p-2 rounded-4" id="portfolio-image"/>
-      <Image src="https://www.picsum.photos/300/200" alt="portfolio image"  className="p-2 rounded-4" id="portfolio-image"/>
-      <Image src="https://www.picsum.photos/400/300" alt="portfolio image" className="p-2 rounded-4" id="portfolio-image"/>
+      <Image src={portfolio.portfolioImageUrl} alt="portfolio image" className="p-2 rounded-4" id="portfolio-image"/>
+      {/*<Image src="https://www.picsum.photos/250/200" alt="portfolio image"  className="p-2 rounded-4" id="portfolio-image"/>*/}
+      {/*<Image src="https://www.picsum.photos/300/200" alt="portfolio image"  className="p-2 rounded-4" id="portfolio-image"/>*/}
+      {/*<Image src="https://www.picsum.photos/400/300" alt="portfolio image" className="p-2 rounded-4" id="portfolio-image"/>*/}
     </>
   )
 }
+
