9f4da5da7330c9f1a09215ba5897d43969f557ef
Jordyn Palma <Jordynpalmadesign@gmail.com>
2023-03-14 17:09:59 -0600

    adjusted profile card styling so that image fills frame

--- frontend/src/ui/landing-page/componeents/ProfileCards.jsx
+++ frontend/src/ui/landing-page/componeents/ProfileCards.jsx
@@ -1,16 +1,18 @@
 import React from "react";
-import {Card, Col, Image} from "react-bootstrap";
+import {Card, Col, Container, Image} from "react-bootstrap";
 import "../LandingPage.css"
 export function ProfileCards(props) {
     const {allProfiles} = props
     return (
 
         <>
-            <Col className='my-3' xs={6} md={4} lg={3}>
+            <Col className='my-3' xs={6} sm={6} md={4} lg={3}>
                 <Card id='profile-card'>
                     <Card.Link id="home-card-link" href="/maker-profile">
-                        <Image id="home-profile-card-image" fluid variant="top" src={allProfiles.profileImageUrl}/>
-                        <Card.Body className='p-2'>
+                        <Container id="home-profile-card-image" className='d-flex p-0 m-0'>
+                        <Image fluid variant="top" src={allProfiles.profileImageUrl}/>
+                        </Container>
+                        <Card.Body>
                             <Card.Title className='fs-6'>{allProfiles.profileFullName}</Card.Title>
                         </Card.Body>
                     </Card.Link>
