654056c5dff2ac5f57e3dc11e8f98ecb7c54e5a9
Damon Ortega <dortega28@cnm.edu>
2023-03-15 14:33:42 -0600

    formated the submit button and made updates to form labels

--- frontend/src/ui/CommunityAccount/EditCommunityAccountForm.jsx
+++ frontend/src/ui/CommunityAccount/EditCommunityAccountForm.jsx
@@ -13,10 +13,8 @@ export const EditCommunityAccountForm = (props) => {
 
     const validationObject = Yup.object().shape({
 
-        firstName: Yup.string()
-            .max ( 16, 'Input less than 16 characters' ),
-        lastName: Yup.string()
-            .max ( 16, 'Input less than 16 characters' ),
+        fullName: Yup.string()
+            .max ( 32, 'Input less than 32 characters' ),
         profileEmail: Yup.string()
             .email( 'Email must be a valid email' ),
         profileName: Yup.string()
@@ -71,6 +69,7 @@ export const EditCommunityAccountForm = (props) => {
             touched,
             dirty,
             isSubmitting,
+            isValid,
             handleChange,
             handleBlur,
             handleSubmit,
@@ -80,51 +79,32 @@ export const EditCommunityAccountForm = (props) => {
         return (
             <>
                 <Form onSubmit={handleSubmit}>
-                    <h1 className="text-center text-light py-5">Account Settings</h1>
+                    <h1 className="text-center text-light py-5">Update Account Information</h1>
                 {/*controlId must match what is passed to the initialValues prop*/}
-                <Form.Group className="mb-4 px-3" controlId="firstName">
-                    <Form.Label>first name</Form.Label>
+                <Form.Group className="mb-4 px-3" controlId="fullName">
+                    <Form.Label>Enter your full name</Form.Label>
                     <InputGroup>
                         <InputGroup.Text>
-                            <FontAwesomeIcon icon="envelope"/>
+                            {/*<FontAwesomeIcon icon="envelope"/>*/}
                         </InputGroup.Text>
                         <FormControl
                             className="form-control"
-                            name="firstName"
+                            name="fullName"
                             type="text"
-                            value={values.firstName}
-                            placeholder="First Name"
+                            value={values.fullName}
+                            placeholder="Full Name"
                             onChange={handleChange}
                             onBlur={handleBlur}
 
                         />
                     </InputGroup>
-                    <DisplayError errors={errors} touched={touched} field={'firstName'}/>
-                </Form.Group>
-                <Form.Group className="mb-4 px-3" controlId="lastName">
-                    <Form.Label>last name</Form.Label>
-                    <InputGroup>
-                        <InputGroup.Text>
-                            <FontAwesomeIcon icon="envelope"/>
-                        </InputGroup.Text>
-                        <FormControl
-                            className="form-control"
-                            name="lastName"
-                            type="text"
-                            value={values.lastName}
-                            placeholder="Last Name"
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-
-                        />
-                    </InputGroup>
-                    <DisplayError errors={errors} touched={touched} field={'lastName'}/>
+                    <DisplayError errors={errors} touched={touched} field={'fullName'}/>
                 </Form.Group>
                 <Form.Group className="mb-4 px-3" controlId="profileEmail">
-                    <Form.Label>first name</Form.Label>
+                    <Form.Label>Update your e-mail address</Form.Label>
                     <InputGroup>
                         <InputGroup.Text>
-                            <FontAwesomeIcon icon="envelope"/>
+                            {/*<FontAwesomeIcon icon="envelope"/>*/}
                         </InputGroup.Text>
                         <FormControl
                             className="form-control"
@@ -140,10 +120,10 @@ export const EditCommunityAccountForm = (props) => {
                     <DisplayError errors={errors} touched={touched} field={'profileEmail'}/>
                 </Form.Group>
                 <Form.Group className="mb-4 px-3" controlId="userName">
-                    <Form.Label>first name</Form.Label>
+                    <Form.Label>Username</Form.Label>
                     <InputGroup>
                         <InputGroup.Text>
-                            <FontAwesomeIcon icon="envelope"/>
+                            {/*<FontAwesomeIcon icon="envelope"/>*/}
                         </InputGroup.Text>
                         <FormControl
                             className="form-control"
@@ -159,10 +139,10 @@ export const EditCommunityAccountForm = (props) => {
                     <DisplayError errors={errors} touched={touched} field={'userName'}/>
                 </Form.Group>
                 <Form.Group className="mb-4 px-3" controlId="profilePassword">
-                    <Form.Label>first name</Form.Label>
+                    <Form.Label>Update your password</Form.Label>
                     <InputGroup>
                         <InputGroup.Text>
-                            <FontAwesomeIcon icon="envelope"/>
+                            {/*<FontAwesomeIcon icon="envelope"/>*/}
                         </InputGroup.Text>
                         <FormControl
                             className="form-control"
@@ -178,10 +158,10 @@ export const EditCommunityAccountForm = (props) => {
                     <DisplayError errors={errors} touched={touched} field={'profilePasswordConfirm'}/>
                 </Form.Group>
                 <Form.Group className="mb-4 px-3" controlId="profilePasswordConfirm">
-                    <Form.Label>first name</Form.Label>
+                    <Form.Label>Confirm your updated password</Form.Label>
                     <InputGroup>
                         <InputGroup.Text>
-                            <FontAwesomeIcon icon="envelope"/>
+                            {/*<FontAwesomeIcon icon="envelope"/>*/}
                         </InputGroup.Text>
                         <FormControl
                             className="form-control"
@@ -198,14 +178,16 @@ export const EditCommunityAccountForm = (props) => {
                 </Form.Group>
 
                 <Form.Group className="d-flex justify-content-center">
-                    <Button variant="light" type="submit" className="mb-3">Submit changes</Button>
-                    {' '}
+                    <Button variant="light" type="submit" className='m-2 btn btn-danger mb-3 fw-bold border border-dark border-2'
+                        onClick={handleSubmit}
+                        disabled={!isValid || isSubmitting || (Object.keys(touched).length === 0 && touched.constructor === Object)}
+                    >Submit changes</Button>
+                    {" "}
                     <Button
-                        className="btn btn-danger"
+                        className='m-2 btn btn-danger mb-3 fw-bold border border-dark border-2'
                         onClick={handleReset}
                         disabled={!dirty || isSubmitting}
-                    >Reset
-                    </Button>
+                    >Reset</Button>
                 </Form.Group>
                 </Form>
                 <DisplayStatus status={status}/>
