8b2459006a9076875f3be20b614d7ffbd4efe192
Joel Rinderknecht <jrinderknecht@cnm.edu>
2023-03-14 19:59:49 -0600

    created signupcontent file

--- frontend/src/ui/maker-profile/MakerProfile.jsx
+++ frontend/src/ui/maker-profile/MakerProfile.jsx
@@ -1,32 +1,48 @@
 import {Col, Container, Image, Row} from "react-bootstrap";
-import {PortfolioImage} from "./PortfolioImage.jsx";
+import {PortfolioImages} from "./PortfolioImage.jsx";
 import React from "react";
 import {Skill} from "./Skill.jsx";
 import "../App.css"
 import "./MakerProfile.css"
-import {fetchProfileByProfileId} from "../../store/profiles.js";
+import profiles, {fetchProfileByProfileId} from "../../store/profiles.js";
+import {useDispatch, useSelector} from "react-redux";
+import * as allPortfolios from "react-bootstrap/ElementChildren";
 
-fetchProfileByProfileId()
 export function MakerProfile() {
 
+  const portfolioImages = useSelector ( state => {
+    if (state?.profiles.constructor.name === "Object") {
+      return Object.values(state.profiles)
+    } else []
+  })
+
+  const dispatch = useDispatch()
+
+  const initialEffect = () => {
+    dispatch ( fetchProfileByProfileId() )
+  }
+
+  React.useEffect (initialEffect, [])
+
+  console.log (profiles)
 
   return (
     <>
       <Container className="p-5 ps-lg-0 mt-5 mx-auto rounded-4" id="background-about-me">
         <Row>
           <Col className="d-flex align-content-center">
-            <Image roundedCircle height={250} width={250} src={profile.profileImageUrl}
+            <Image roundedCircle height={250} width={250} src="https://bootdey.com/img/Content/avatar/avatar7.png"
                    alt="profile picture" className=" d-block mx-auto mb-lg-0 mb-3" id="profile-image"/>
           </Col>
 
           <Col lg={6} className="order-3 order-sm-2 text-light">
-            <h1 className="text-center text-md-start" id="profile-name">{profile.profileName}</h1>
+            <h1 className="text-center text-md-start" id="profile-name">profileName</h1>
             <p className="mt-3" id="about-me-text">profileAboutMe: As High as Honor. The winds of Winter. Never Resting. Can a
               man still be brave if he’s afraid? That is the only time a man can be brave.Forgive my manners. I don't
               see many ladies these days. Lucky for the ladies. A forked purple lightning bolt, on black field speckled
               with four-pointed stars. The tourney of Ashford Meadows. It's ten thousand miles between Kings landing and
               the wall.</p>
-            <p id="email-text">{profile.profileEmail}</p>
+            <p id="email-text">profile profileEmail</p>
           </Col>
 
           <Col className="order-2 order-sm-3 my-3 mt-lg-0 d-block d-lg-inline">
@@ -39,12 +55,12 @@ export function MakerProfile() {
       <Container>
         <Row>
           <Col className="my-3">
-            <PortfolioImage />
+            { allPortfolios.map (allPortfolios => <PortfolioImages allPortfolios={allPortfolios} key={allPortfolios.profileId}
+            />)}
           </Col>
-
           <Col xs={12} md={4} className="rounded-4 p-4 mt-4 mb-auto text-white" id="pricing">
             <h2 className="text-md-center text-sm-start">Pricing:</h2>
-            <p className=" mt-3">{profile.profilePricing} Jin ave sekke verven anni m'orvikoon. Hash yer dothrae chek asshekh?
+            <p className=" mt-3">profile.profilePricing Jin ave sekke verven anni m'orvikoon. Hash yer dothrae chek asshekh?
               Ki fin yeni?</p>
           </Col>
         </Row>
--- frontend/src/ui/sign-up/SignUp.jsx
+++ frontend/src/ui/sign-up/SignUp.jsx
@@ -1,18 +1,22 @@
-import { Button, Container, Form } from "react-bootstrap"
+import {Button, Container, Form, InputGroup} from "react-bootstrap"
 import "./SignUp.css"
 import React from "react"
 import { httpConfig } from "../shared/utils/httpconfig"
 import { Formik } from "formik"
 import * as Yup from "yup"
 import {Link} from "react-router-dom";
-import {useDispatch} from "react-redux";
-import { DisplayError } from "../shared/components/display-error/DisplayError"
-import { DisplayStatus } from "../shared/components/display-status/display-status.jsx";
+import {SignUpFormContent} from "./SignUpContent.jsx";
 
-export function SignUp() {
+export const SignUp = () => {
 
-  const { signUpValues } = ''
-  const dispatch = useDispatch ()
+  const signUpValues = {
+    profileEmail: "",
+    profileFullName: "",
+    profilePassword: "",
+    profilePasswordConfirm: "",
+    profileIsMaker: "",
+    profileName:""
+  }
 
   const validator = Yup.object().shape ({
     profileEmail: Yup.string ()
@@ -26,113 +30,32 @@ export function SignUp() {
       .min ( 8, 'Password must be at least eight characters'),
     profilePasswordConfirm: Yup.string ()
       .required ( 'Password confirmation is required'),
-      //. must be same as password
-    profileIsMaker: Yup.string () //Yup...
+    profileIsMaker: Yup.boolean ()
   .required ( 'Account Type must be selected'),
-  //radio button
     profileName: Yup.string ()
     .required ( 'Username is required')
       .max ( 64, 'Username can not be more than 64 characters')
-    //must be unique
   })
 
   const submitSignUp = ( values, { resetForm, setStatus }) => {
-    httpConfig.post (" apis/sign-up ", values)
-      .then (reply => {
-        let { message, type } = reply
-        setStatus ({ message, type })
-        if ( reply.status === 200 ) {
-          window.localStorage.removeItem
+    httpConfig.post("/apis/sign-up", values)
+      .then(reply => {
+          let {message, type} = reply
+          if (reply.status === 200) {
+            resetForm();
+          }
+          setStatus({message, type})
         }
-
-      })
+      )
   }
 
+  console.log(signUpValues)
+
 return (
-  <>
-    <Container className="bg-light mt-5 rounded-4 border border-dark border-3" id="sign-up-form">
-      <h1 className="text-center text-light py-5" id="sign-up">Sign-Up</h1>
-    <Formik initialValues={ signUpValues }
+    <Formik initialValues= { signUpValues }
             onSubmit= { submitSignUp }
             validationSchema = { validator }>
-      { SignUpFormContent }
+      < SignUpFormContent />
     </Formik>
-
-      <Container className='my-4 text-center'>
-        <h4>Already a member?</h4>
-        <Link to='/sign-in'><h5>Sign In HERE</h5></Link>
-      </Container>
-  </Container>
-  </>
 )
-
-
-}
-
-function SignUpFormContent (props) {
-
-  const {
-    status,
-    values,
-    errors,
-    dirty,
-    isSubmitting,
-    handleChange,
-    handleBlur,
-    handleSubmit,
-    handleReset
-  } = props
-
-
-  return (
-    <>
-<Form onSubmit = { handleSubmit } >
-
-          <Form.Group className="my-3 px-3 ">
-            <Form className="bg-light rounded-2 align-content-center w-50 border border-dark border-2 ps-3">
-              {['radio'].map((type) => (
-                <div key={`inline-${type}`} className="pt-2">
-                  <Form.Label inline className="col-12 col-lg-4 fw-bold pe-3">Account Type:</Form.Label>
-                  <Form.Check inline className="col-12 col-lg-5" label="Community Member" name ="group1" type={type} id={`inline-${type}`} />
-                  <Form.Check inline label="Maker" name ="group1" type={type} id={`inline-${type}`}/>
-                </div>
-              ))}
-            </Form>
-          </Form.Group>
-
-          <Form.Group className="mb-4 px-3">
-            <Form.Control type="name" placeholder="Enter your full name" className="fw-bold border border-dark border-2" maxLength={32} id="placeholder-text"/>
-          </Form.Group>
-
-          <Form.Group className="mb-4 px-3">
-            <Form.Control type="username" placeholder="Enter your preferred username" className="fw-bold border border-dark border-2" maxLength={32} id="placeholder-text" />
-          </Form.Group>
-
-          <Form.Group className="mb-4 px-3">
-            {/*<Form.Label className="text-light fw-bold">New password:</Form.Label>*/}
-            <Form.Control type="password" placeholder="Enter your new password" className="fw-bold border border-dark border-2" minLength={8} id="placeholder-text" />
-            <Form.Text className="text-light">
-              Please enter a password that is at least 8 characters long
-            </Form.Text>
-          </Form.Group>
-
-          <Form.Group className="mb-4 px-3">
-            {/*<Form.Label className="text-light fw-bold">Confirm password:</Form.Label>*/}
-            <Form.Control type="password" placeholder="Confirm your new password" className="fw-bold border border-dark border-2" minLength={8} id="placeholder-text" />
-          </Form.Group>
-
-          <Form.Group className="mb-4 px-3">
-            {/*<Form.Label className="text-light fw-bold">E-mail:</Form.Label>*/}
-            <Form.Control type="email" placeholder="Enter your e-mail address" className="fw-bold border border-dark border-2" id="placeholder-text"/>
-          </Form.Group>
-
-          <Form.Group className="d-flex justify-content-center">
-            <Button variant="light" type="submit" className="mb-3 fw-bold border border-dark border-2">Submit</Button>
-          </Form.Group>
-
-      </Form>
-    </>
-  )
-
-
 }
