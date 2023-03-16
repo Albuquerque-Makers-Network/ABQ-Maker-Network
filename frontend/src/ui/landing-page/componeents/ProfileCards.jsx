2261e4d0f8bf140fa7b84ef7a76059212a54e4ab
Jordyn Palma <Jordynpalmadesign@gmail.com>
2023-03-16 13:16:52 -0600

    quick fix to card col

--- frontend/src/ui/landing-page/componeents/ProfileCards.jsx
+++ frontend/src/ui/landing-page/componeents/ProfileCards.jsx
@@ -9,18 +9,18 @@ export function ProfileCards(props) {
 
   return (
     <>
-<Link to={`/maker-profile/${allProfiles.profileId}`} className='col my-3' xs={6} sm={6} md={4} lg={3}>
-      <Card id='profile-card'>
-        <Card.Link id="home-card-link" href="/maker-profile">
-          <Container id="home-profile-card-image" className='d-flex p-0 m-0'>
-            <Image fluid variant="top" src={allProfiles.profileImageUrl}/>
-          </Container>
-          <Card.Body>
-            <Card.Title className='fs-6'>{allProfiles.profileFullName}</Card.Title>
-          </Card.Body>
-        </Card.Link>
-      </Card>
-</Link>
+      <Col xs={6} sm={6} md={4} lg={3}>
+        <Link to={`/maker-profile/${allProfiles.profileId}`} className='my-3' >
+            <Card id='profile-card'>
+                <Container id="home-profile-card-image" className='d-flex p-0 m-0'>
+                  <Image fluid variant="top" src={allProfiles.profileImageUrl}/>
+                </Container>
+                <Card.Body>
+                  <Card.Title className='fs-6'>{allProfiles.profileFullName}</Card.Title>
+                </Card.Body>
+            </Card>
+        </Link>
+      </Col>
 </>
 )
 }
