b1476892cfa599c34cf1e4fd5a3fadeb3326f28c
Joel Rinderknecht <jrinderknecht@cnm.edu>
2023-03-17 09:39:07 -0600

    modified skill route

--- backend/src/apis/skill/skill.route.ts
+++ backend/src/apis/skill/skill.route.ts
@@ -20,7 +20,7 @@ skillsRouter.route('/:skillId')
 skillsRouter.route('/SkillsIsPopular/Popular')
     .get(getAllIsPopularSkillsController)
 
-skillsRouter.route('/profileId/:profileId')
+skillsRouter.route('/:profileId')
   .get(asyncValidatorController([check
   ('profileId', 'please provide a valid profileId').isUUID()])
 , getSkillByProfileIdController
