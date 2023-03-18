f399aca7f2b47b7d3e9efe3dd297afb891502983
Jordyn Palma <Jordynpalmadesign@gmail.com>
2023-03-18 14:16:42 -0600

    added account redirection links on nav bar for profileismaker is true

--- frontend/src/ui/shared/components/NavBar/SignedIn.jsx
+++ frontend/src/ui/shared/components/NavBar/SignedIn.jsx
@@ -1,23 +1,29 @@
 import {Image, Nav, Container} from "react-bootstrap";
-import ProfilePicture from "../../../../assets/profile-placeholder.png";
 import {SignOut} from "./SignOut.jsx";
+import {useSelector} from "react-redux";
 
 export const SignedIn = (props) => {
 
     const { profile } = props
-
+    const handleAccountRedirect = () => {
+        if (profile.profileIsMaker === true) {
+            window.location.replace('/maker-account')
+        } else {
+            window.location.replace('/community-account')
+        }
+    }
 
     return (
         <>
             <Nav className="ms-auto d-flex">
                 <Nav.Link className='py-0' href="/">Home</Nav.Link>
                 {/*image displays on full screen, Account page text displays on mobile*/}
-                <Nav.Link id="nav-account" href="/maker-account">Account Page</Nav.Link>
+                <Nav.Link id="nav-account" onClick={handleAccountRedirect}>Account Page</Nav.Link>
                 <Container className="p-0 m-0 text-lg-end">
-                    <Nav.Link className='py-0' href="/maker-account">Signed in as: {profile.profileName}</Nav.Link>
+                    <Nav.Link className='py-0' onClick={handleAccountRedirect}>Signed in as: {profile.profileName}</Nav.Link>
                     <SignOut/>
                 </Container>
-                <Nav.Link id='nav-pic' href="/maker-account">
+                <Nav.Link id='nav-pic' onClick={handleAccountRedirect}>
                     <Image fluid className='border border-0 rounded-3' src={profile.profileImageUrl} width='50'/>
                 </Nav.Link>
             </Nav>
